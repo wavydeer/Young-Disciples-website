076ad6a5257f8406896cc047e7e1c5c2a31eb19c
styled <smyvensestime@gmail.com>
2021-11-13 12:15:29 -0500

    submit handler for donate

--- assets/js/donate.js
+++ assets/js/donate.js
@@ -101,72 +101,78 @@
 //     $('.donate__form').trigger('reset')
 // })
 
-// when user submits the form
-$('document').ready(function () {
-    // method for how many characters a filed have
-    jQuery.validator.addMethod("exactlength", function(value, element, param) {
-        return this.optional(element) || value.length == param;
-    }, $.validator.format("Please enter exactly {0} characters."));
+// so everything can be locally scoped, similar to (if __name__ = '__main__')
+(function($, window, document) {
 
-    // method for date "mm/yyyy" fromat
-    jQuery.validator.addMethod("expireformat", function(value, element) {
-        return value.match(/^\d{1,2}\/\d{4}$/);
-    }, "")
+    // when the window is ready this runs
+    $(function() {
+        // method for how many characters a filed have
+        jQuery.validator.addMethod("exactlength", function(value, element, param) {
+            return this.optional(element) || value.length == param;
+        }, $.validator.format("Please enter exactly {0} characters."));
 
-    // form validation
-    $('.form').validate({
-        rules: {
-            amount: {
-                required: true,
-                number: true
+        // method for date "mm/yyyy" fromat
+        jQuery.validator.addMethod("expireformat", function(value, element) {
+            return value.match(/^\d{1,2}\/\d{4}$/);
+        }, "")
+
+        // form validation
+        $('#payment__form').validate({
+            rules: {
+                amount: {
+                    required: true,
+                    number: true
+                },
+                cardNumber: {
+                    required: true,
+                    number: true,
+                    exactlength: 16
+                },
+                expirationDate: {
+                    required: true,
+                    expireformat: true
+                },
+                cvc: {
+                    required: true,
+                    number: true,
+                    exactlength: 3
+                },
+                nameOnCard: {
+                    required: true,
+                    lettersonly: true,
+                    maxlength: 35
+                }
             },
-            cardNumber: {
-                required: true,
-                number: true,
-                exactlength: 16
+            messages: {
+                amount: {
+                    required: "Amount cannot be empty!",
+                    number: "Invalid donation amount!"
+                },
+                cardNumber: "Invalid card number!",
+                expirationDate: "Invalid expiration date!",
+                cvc: "Invalid CVC",
+                nameOnCard: "Enter the name as it appears on the card"
             },
-            expirationDate: {
-                required: true,
-                expireformat: true
+
+            // show errors 1 by 1 in a div
+            showErrors: function(errorMap, errorList) {
+                $("#payment__form").find("input").each(function() {
+                    $(this).removeClass("error");
+                });
+
+                $("#error-message").html("");
+                if (errorList.length) {
+                    $("#error-message").html(errorList[0]['message']);
+                    $(errorList[0]['element']).addClass("error");
+                }
             },
-            cvc: {
-                required: true,
-                number: true,
-                exactlength: 3
-            },
-            nameOnCard: {
-                required: true,
-                lettersonly: true,
-                maxlength: 35
+
+            submitHandler: function(form, event) { 
+                event.preventDefault();
+                alert("Do some stuff...");
+                //submit via ajax
             }
-        },
-        messages: {
-            amount: {
-                required: "Amount cannot be empty!",
-                number: "Invalid donation amount!"
-            },
-            cardNumber: "Invalid card number!",
-            expirationDate: "Invalid expiration date!",
-            cvc : "Invalid CVC",
-            nameOnCard: "Enter the name as it appears on the card"
-        },
 
-        // show errors 1 by 1 in a div
-        showErrors: function(errorMap, errorList) {
-            $(".form").find("input").each(function() {
-                $(this).removeClass("error");
-            });
-
-            $("#error-message").html("");
-            if(errorList.length) {
-                $("#error-message").html(errorList[0]['message']);
-                $(errorList[0]['element']).addClass("error");
-            }
-        },
-
-        // send the form to api to handle request in the backend
-        submitHandler: function(form) {
-            //do submit
-        }
+        })
     })
-})
+}(window.jQuery, window, document))
