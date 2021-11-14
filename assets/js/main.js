0f48f8a8411c7f42215c64413ccf6e8cb0532f7c
styled <smyvensestime@gmail.com>
2021-11-13 22:48:05 -0500

    Co-authored-by: wavydeer <wavydeer@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 // wait for whole page to load
 $(window).on('load', function() {
     //remove # from url address bar
-    $(window).on('hashchange', function(e){
-        history.replaceState ("", document.title, e.originalEvent.oldURL)
+    $(window).on('hashchange', function(e) {
+        history.replaceState("", document.title, e.originalEvent.oldURL)
     })
 
     // make default <a href="#"> tag not do anything
-    $('a[href="#"]').click(function (e) {
+    $('a[href="#"]').click(function(e) {
         e.preventDefault();
     })
 })
@@ -14,12 +14,15 @@ $(window).on('load', function() {
 // =========================================================
 
 // load navbar and footer
-$(function(){
+$(function() {
     $("#navbar").load("template/navbar.html")
 })
 
-$(function(){
+$(function() {
     $("#footer").load("template/footer.html")
+})
+$(function() {
+    $("#page-header").load("template/page-header.html")
 })
 
 // make sure the nav bar is always on top of everything
@@ -30,4 +33,26 @@ navbar.style.zIndex = 10000
 // =========================================================
 
 // ensure that style.css is loaded for the html file to use
-$('head').append('<link rel="stylesheet" type="text/css" href="/assets/css/style.css">');
+$('head').append(`
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+`)
+
+// ==========================================================
+
+
+// button shows at button of the screen to scroll back to top of page
+$('main').append(`
+    <a href="#" class="scrollup" id="scroll-up">
+        <i class='bx bx-chevron-up scrollup__icon'></i>
+    </a>
+`)
+
+$(window).scroll(function() {
+    if (this.scrollY >= 200) $('#scroll-up').addClass('show-scroll')
+    else $('#scroll-up').removeClass('show-scroll')
+})
+
+$("#scroll-up").click(function() {
+    $(window).scrollTop(0);
+});
