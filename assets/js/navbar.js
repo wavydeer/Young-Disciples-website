278f50961f880d0c757a90df4799f003a4917562
styled <smyvensestime@gmail.com>
2021-12-07 00:00:46 -0500

    minor code reformatting

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -12,20 +12,20 @@ const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+const getCurrentTheme = () => $('body').hasClass(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => $(themeButton).hasClass(iconTheme) ? 'bx-moon' : 'bx-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-$('#theme-button').click(function () {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+$(themeButton).click(function() {
+    $('body').toggleClass(darkTheme)
+    $(themeButton).toggleClass(iconTheme)
+        // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
@@ -33,16 +33,16 @@ $('#theme-button').click(function () {
 // ==================================================================
 
 // show and hide the menu
-$('#nav-toggle').click(function () {
+$('#nav-toggle').click(function() {
     $('#nav-menu').addClass('show-menu')
 })
-$('#nav-close').click(function () {
+$('#nav-close').click(function() {
     $('#nav-menu').removeClass('show-menu')
 })
 
 // hide menu when you click on an item mobile
-$('.nav__link').each(function () {
-    $(this).click(function () {
+$('.nav__link').each(function() {
+    $(this).click(function() {
         $('#nav-menu').removeClass('show-menu')
     })
 })
