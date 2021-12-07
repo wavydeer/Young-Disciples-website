72c6c853b9363f5f8be604c52f4bc1f31e82ad1a
styled <smyvensestime@gmail.com>
2021-12-06 23:40:15 -0500

    changed the way individuals are stored

--- assets/js/meet-us.js
+++ assets/js/meet-us.js
@@ -1,117 +1,24 @@
 const imagelocation = 'assets/img/'
 
-const individuals = 
-[
-    {
-        name: 'Pauldette Dor',
-        proffession: 'President',
-        description: 'As President I oversee all ministry programs directed specifically within the Youth. I lead in the development, coordinatation and lead of all church youth activities.',
-        profilePicture: imagelocation + 'Pauldette-profile.PNG',
-        backgroundPicture: imagelocation + 'Pauldette-background.jpeg'
-    },
-    {
-        name: 'Derek Polydor',
-        proffession: 'Vice President',
-        description: 'Being second in command, I assist in all of the roles of the president while also maintaining control over certain ministries like the Mens Ministries. While also being a guide to others.',
-        profilePicture: imagelocation + 'Derek-profile.jpeg',
-        backgroundPicture: imagelocation + 'Derek-background1.jpeg'
-    },
-    {
-        name: 'Jenny Lesly',
-        proffession: 'Secretary',
-        description: 'I typically handle all the scheduling of events, attendance, and records of things that are going to happen and things that have already happened. While assiting where it sees fit.',
-        profilePicture: imagelocation + 'Jenny-profile.PNG',
-        backgroundPicture: imagelocation + 'Jenny-background.jpeg'
-    },
-    {
-        name: 'Eluid Philip',
-        proffession: 'Lead Treasurer',
-        description: 'As lead, I maintain control and advise with the financial decision of the youth. Planning fundraisers and events are ways we benefit in making money for the youth.',
-        profilePicture: imagelocation + 'Philip-profile.JPG',
-        backgroundPicture: imagelocation + 'Philip-background.jpeg'
-    },
-    {
-        name: 'Jessica Cherivil',
-        proffession: 'Chior Director',
-        description: 'I am the overseer of the chior. I plan meetings, teach and practice with the team. I am the lead when performing at events and in house, directing the notes and the sequence of the songs.',
-        profilePicture: imagelocation + 'Jessica-profile.jpg',
-        backgroundPicture: imagelocation + 'Jessica-background.jpeg'
-    },
-    {
-        name: 'Junior Marseille',
-        proffession: 'Youth Preacher',
-        description: 'My roles include being the preacher for the youth while also teaching other preachers. When Im not doing that playing the guitar for the directors is my passion.',
-        profilePicture: imagelocation + 'Junior-profile.JPG',
-        backgroundPicture: imagelocation + 'Junior-Background.PNG'
-    },
-    {
-        name: 'Evona Prophet',
-        proffession: 'Praise Team Leader',
-        description: 'As praise team leader, I guide, teach and sing with those chosen to be apart of the group. I also assit in the treasury department for the youth and the main church.',
-        profilePicture: imagelocation + 'Evona-profile.jpg',
-        backgroundPicture: imagelocation + 'Evona-background.PNG'
-    },
-    {
-        name: 'Bienta Bien-Aime',
-        proffession: 'Event Coordinator',
-        description: 'When the youth decideds to take a trip I am usually the one on duty making the arrangements. I also help other with financial decisions and life advice.',
-        profilePicture: imagelocation + 'Bienta-profile.jpeg',
-        backgroundPicture: imagelocation + 'Bienta-background.jpeg'
-    },
-    {
-        name: 'Malcolm Verdier',
-        proffession: 'Treasurer/Advisor',
-        description: 'I assist with the youth treasury, while also being an advisor on college and financial dealings. I carry a seat in the decision making when there are any changed within the youth.',
-        profilePicture: imagelocation + 'Malcolm-profile.PNG',
-        backgroundPicture: imagelocation + 'Malcolm-background1.jpg'
-    },
-    {
-        name: 'George Blaise',
-        proffession: 'Youth Male Coulselor',
-        description: 'As counselor, it became my duty to always check on the men youth on a personal level to make sure we can get to the root of any problems. On my free time I also play the drums.',
-        profilePicture: imagelocation + 'George-profile.PNG',
-        backgroundPicture: imagelocation + 'George-background.JPG'
-    },
-    {
-        name: 'Fallande Joseph',
-        proffession: 'Youth Women Counselor',
-        description: 'I am an ear that will listen so the women can having someone to confide in and trust. I keep tabs and check up on the women of the youth to make sure there is always help through tough times.',
-        profilePicture: imagelocation + 'Fallande-profile.jpeg',
-        backgroundPicture: imagelocation + 'Fallande-background.jpeg'
-    },
-    {
-        name: 'Lusie Pierre',
-        proffession: 'Counselor',
-        description: 'I assist where ever is needed within the church. When I am on my free time I am as a counselor to those going through tough times and serious issues.',
-        profilePicture: imagelocation + 'Lusie-profile.jpeg',
-        backgroundPicture: imagelocation + 'Lusie-background.jpeg'
-    },
-    {
-        name: 'Patrick Dor',
-        proffession: 'Transportation Lead',
-        description: 'I am responisble for taking and bring the youth back and forth from the church. I manage the schedules of who is driving the bus and when people need to be picked up.',
-        profilePicture: imagelocation + 'Patrick-profile.jpeg',
-        backgroundPicture: imagelocation + 'Patrick-background.jpeg'
-    },
-]
+$.getJSON('assets/js/meet-us.json', (data) => {
+    $(data.individuals).each((index, item) => {
+        $('.card-wrapper').append(`
+            <div class="card" data-person="${item.name}">
+                <img src= ${imagelocation}${item.backgroundPicture} class="card-image">
+                <img src= ${imagelocation}${item.profilePicture} class="profile-image">
+                <h1 class="card-title">${item.name}</h1>
+                <p class="job-title">${item.proffession}</p>
 
-$(individuals).each(function(key, value) {
-    $('.card-wrapper').append(`
-        <div class="card" data-person="${value.name}">
-            <img src= ${value.backgroundPicture} class="card-image">
-            <img src= ${value.profilePicture} class="profile-image">
-            <h1 class="card-title">${value.name}</h1>
-            <p class="job-title">${value.proffession}</p>
+                <div class="about-container">
+                    <p class="about">
+                        ${item.description}
+                    </p>
+                </div>
 
-            <div class="about-container">
-                <p class="about">
-                    ${value.description}
-                </p>
+                <a href="#ok" class="btn">Contact</a> 
             </div>
-
-            <a href="#ok" class="btn">Contact</a> 
-        </div>
-    `)
+        `)
+    })
 })
 
 $('.card .btn').click(function () {
