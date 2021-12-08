0b96e89b01e66f29c2f5102a126b7dd4d362a89f
styled <smyvensestime@gmail.com>
2021-12-08 16:56:48 -0500

    make sure that the file is being ran and not impor

--- scripts/main.py
+++ scripts/main.py
@@ -5,15 +5,21 @@ format pictures from heavy file formats such as png, jpeg... to a light weight f
 import os
 import json
 
-webp_dir = 'D:\\programing\\libwebp-1.2.1-windows-x64\\libwebp-1.2.1-windows-x64\\bin\\cwebp'
-input_dir = lambda directory : f'D:\\programing\\HTML\\Young-Disciples-website\\assets\img\\{directory}'
-output_dir = lambda directory : f'D:\\programing\\HTML\\Young-Disciples-website\\assets\img\\{directory.find(".")}.webp'
 
-with open('D:\\programing\\HTML\\Young-Disciples-website\\assets\\js\\meet-us.json', 'r') as file:
-    data = json.load(file)
-    # loop through json object in file
-    for individual in data['individuals']:
-        # check if the file has already been formatted
-        if output_dir(individual['backgroundPicture'])[-5:] != '.webp':
-            # run through formatting software and output every file to .webp
-            os.system(f'{webp_dir} {input_dir(individual["backgroundPicture"])} -q 80 -o {output_dir(individual["backgroundPicture"])}')
+def main():
+    webp_dir = 'D:\\programing\\libwebp-1.2.1-windows-x64\\libwebp-1.2.1-windows-x64\\bin\\cwebp'
+    input_dir = lambda directory : f'D:\\programing\\HTML\\Young-Disciples-website\\assets\img\\{directory}'
+    output_dir = lambda directory : f'D:\\programing\\HTML\\Young-Disciples-website\\assets\img\\{directory.find(".")}.webp'
+
+    with open('D:\\programing\\HTML\\Young-Disciples-website\\assets\\js\\meet-us.json', 'r') as file:
+        data = json.load(file)
+        # loop through json object in file
+        for individual in data['individuals']:
+            # check if the file has already been formatted
+            if output_dir(individual['backgroundPicture'])[-5:] != '.webp':
+                # run through formatting software and output every file to .webp
+                os.system(f'{webp_dir} {input_dir(individual["backgroundPicture"])} -q 80 -o {output_dir(individual["backgroundPicture"])}')
+
+
+if __name__ == '__main__':
+    main()
