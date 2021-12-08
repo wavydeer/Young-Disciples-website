2e6c4085f77431c1f6b18a02296de180aba454ad
styled <smyvensestime@gmail.com>
2021-12-08 16:55:54 -0500

    fixed meta tags for firefox

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ $(window).on('load', () => {
 * Load the third party fonts and icon
 */
 
-$('head').append(`
+$('head').prepend(`
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Malcom | Smyvens">
@@ -33,15 +33,14 @@ $('head').append(`
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque autem possimus optio impedit laborum
                 perspiciatis rerum nisi, quam reiciendis voluptas, veniam aliquid eaque? Amet totam ipsam iure voluptatum nobis."
     >
-    <meta http-equiv="Content-Security-Policy" content="
-        default-src 'self';
-        connect-src 'self' https://ka-f.fontawesome.com/releases/v5.15.4/css/;
-        script-src-elem 'self' 'unsafe-inline' https://kit.fontawesome.com/;
-        style-src 'self' 'unsafe-inline';
-        style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com/;
-        font-src 'self' https://fonts.gstatic.com/s/ https://unpkg.com/boxicons@2.0.9/fonts/ https://ka-f.fontawesome.com/releases/v5.15.4/webfonts/
-    ">
-
+    <meta http-equiv="Content-Security-Policy" 
+        content="
+            default-src 'self';
+            connect-src 'self' https://ka-f.fontawesome.com/releases/v5.15.4/css/;
+            script-src 'self' 'unsafe-inline' https://kit.fontawesome.com/;
+            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/ https://unpkg.com/boxicons@2.0.9/css/;
+            font-src 'self' https://fonts.gstatic.com/s/ https://unpkg.com/boxicons@2.0.9/fonts/ https://ka-f.fontawesome.com/releases/v5.15.4/webfonts/"
+    >
     <link rel="icon" type="image/png" href="/assets/img/placeholder.png"/>   
     <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
@@ -55,7 +54,7 @@ $('head').append(`
 $(() => {
     // load navbar
     $.get("template/navbar.html", (data) => {
-        $("#navbar").append(data);
+        $("#navbar").append(data)
 
         // make sure the navbar is always on top of everthing
         $('#navbar').css({
@@ -64,20 +63,24 @@ $(() => {
         })
 
         // highlight the link of the loaded file on the navbar
-        var currentFile = (location.pathname.match(/[^\/]+$/)[0]).split('.')[0]
-        $(`#nav__link-${currentFile}`).addClass('active-link')
-    });
+        if (location.pathname === '/') {
+            $(`#nav__link-index`).addClass('active-link')
+        } else {
+            var currentFile = (location.pathname.match(/[^\/]+$/)[0]).split('.')[0]
+            $(`#nav__link-${currentFile}`).addClass('active-link')
+        }
+    })
 
     // load footer
     $.get("template/footer.html", (data) => {
-        $("#footer").append(data);
-    });
+        $("#footer").append(data)
+    })
 
     // load header
     $.get("template/page-header.html", (data) => {
-        $("#page-header").append(data);
-    });
-});
+        $("#page-header").append(data)
+    })
+})
 
 // #############################################################
 // scroll up button
