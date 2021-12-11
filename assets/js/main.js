cc06ae43c35aba9c9d24491c17f3821db6aaaa5a
styled <smyvensestime@gmail.com>
2021-12-10 21:46:13 -0500

    npm scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,4 @@ const scrollBtnHandler = (() => {
         $(window).scrollTop(0);
     });
 })()
+
--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-import'),  // combine all css to 1 output
         require('autoprefixer'),  // add things like moz-display-flex
+        require('precss'),
         require('cssnano'),  // auto minify css
     ],
 }
