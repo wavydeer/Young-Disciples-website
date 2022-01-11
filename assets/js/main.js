c7719086810a32139914afb2dc6e60246ccfe958
styled <smyvensestime@gmail.com>
2022-01-11 16:08:50 -0500

    color theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@
 const modifyHead = (() => {
     $('head').prepend(`
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/> 
         <meta name="author" content="Malcolm | Smyvens">
         <link rel="icon" type="image/png" href="assets/img/Young-Disciples-logo.png"/>   
         <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
